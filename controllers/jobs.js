12d3c62799915d923f98e82783c6bd111811a978
eldad-assaf <eldadux@gmail.com>
2023-09-19 18:23:15 +0300

    getJob

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,12 +1,34 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors/index");
+
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs ");
+  //only the jobs that are associated with the current user
+
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 const getJob = async (req, res) => {
-  res.send("get a single job ");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 const createJob = async (req, res) => {
-  res.send(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
   res.send("update a job ");
 };
