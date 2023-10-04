b39b6c05646710bcce629ebc45e2d49acd704d0d
eldad-assaf <eldadux@gmail.com>
2023-10-04 16:15:18 +0300

    sync

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//extra security packages
+
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -11,21 +19,32 @@ const jobsRouter = require("./routes/jobs");
 //connectDB
 const connectDB = require("./db/connect");
 
-//authenticate user 
+//authenticate user
 
-const authenticateUser = require('./middleware/authentication');
-
+const authenticateUser = require("./middleware/authentication");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const { mongo } = require("mongoose");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100,
+    // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", authenticateUser,jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
